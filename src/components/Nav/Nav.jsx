eb601868be0e3b95a7421862c20e54aa21594244
Minh Tran <mctran98@yahoo.com>
2021-08-15 15:52:11 -0700

    Changed function to jquery and fix nav values

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,10 +4,10 @@ import $ from 'jquery';
 import Resume from '../../assets/Resume.pdf';
 import useStyles from './useStyles.js';
 
-export default function Nav
-({ scrollToAbout, scrollToProject, scrollToContact, aboutRef, projectRef, contactRef }) {
+export default function Nav({ aboutRef, projectRef, contactRef }) {
   const [value, setValue] = useState(0);
   const [label, setLabel] = useState(0);
+  const [click, setClick] = useState(false);
   const classes = useStyles();
 
   useEffect(() => {
@@ -24,44 +24,49 @@ export default function Nav
         $('html, body').animate({ scrollTop: 0 }, 'slow');
         break;
       case 1:
-        scrollToAbout();
+        $('html, body').animate({ scrollTop: aboutRef.current.offsetTop }, 'slow');
         break;
       case 2:
-        scrollToProject();
+        $('html, body').animate({ scrollTop: projectRef.current.offsetTop }, 'slow');
         break;
       case 3:
-        scrollToContact();
+        $('html, body').animate({ scrollTop: contactRef.current.offsetTop }, 'slow');
         break;
       default:
         break;
     };
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [value]);
+  }, [click]);
 
   const handleValue = (e) => {
     if (window.scrollY < aboutRef.current.offsetTop - 200) {
       setLabel(0);
     } else if (window.scrollY >= aboutRef.current.offsetTop - 100 && window.scrollY < projectRef.current.offsetTop - 200) {
       setLabel(1);
+    } else if (window.scrollY >= contactRef.current.offsetTop - 100 || window.scrollY + $(window).height() >= $(document).height() - 100) {
+      setLabel(3);
     } else if (window.scrollY >= projectRef.current.offsetTop - 100 && window.scrollY < contactRef.current.offsetTop - contactRef.current.clientHeight) {
       setLabel(2);
-    } else if (window.scrollY >= contactRef.current.offsetTop - 100 || window.scrollY + contactRef.current.clientHeight > $(document).height() - contactRef.current.clientHeight) {
-      setLabel(3);
     }
+  };
+
+  const handleClick = (e, value) => {
+    setValue(value);
+    setClick(prev => !prev);
   };
 
   return (
     <Box pt={1} pb={1} className={classes.container}>
-        <Box className={label === 0 ? classes.active : classes.label} onClick={e => setValue(0)}>
+        <Box className={label === 0 ? classes.active : classes.label} onClick={e => handleClick(e, 0)}>
           Home
         </Box>
-        <Box className={label === 1 ? classes.active : classes.label} onClick={e => setValue(1)}>
+        <Box className={label === 1 ? classes.active : classes.label} onClick={e => handleClick(e, 1)}>
           About Me
         </Box>
-        <Box className={label === 2 ? classes.active : classes.label} onClick={e => setValue(2)}>
+        <Box className={label === 2 ? classes.active : classes.label} onClick={e => handleClick(e, 2)}>
           Project
         </Box>
-        <Box className={label === 3 ? classes.active : classes.label} onClick={e => setValue(3)}>
+        <Box className={label === 3 ? classes.active : classes.label} onClick={e => handleClick(e, 3)}>
           Contact
         </Box>
         <Box component='a' href={Resume} target='_blank' className={classes.resume}>
